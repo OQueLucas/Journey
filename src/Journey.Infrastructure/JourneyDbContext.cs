8c61fcea790bc5e2d17afbe32a0f7e15033a530d
Lucas Queiroz <lucas.queiroz@atos.net>
2024-07-09 22:06:43 -0300

    feat: persisting data in db and implementing get all endpoint and use case

---
+++ src/Journey.Infrastructure/JourneyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Journey.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journey.Infrastructure;
+public class JourneyDbContext : DbContext
+{
+    public DbSet<Trip> Trips { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=C:\\workspace\\Journey\\JourneyDatabase.db");
+    }
+}
