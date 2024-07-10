2c5c856479d372153becacb262a82981d1d68224
OQueLucas <l_queiroz@outlook.com.br>
2024-07-10 00:03:49 -0300

    feat: implementing Fluent Validators and creating list of errors to response

--- src/Journey.Exception/ExceptionsBase/NotFoundException.cs
+++ src/Journey.Exception/ExceptionsBase/NotFoundException.cs
@@ -4,6 +4,12 @@ namespace Journey.Exception.ExceptionsBase;
 public class NotFoundException : JourneyException
 {
     public NotFoundException(string message) : base(message) { }
+
+    public override IList<string> GetErrorMessages()
+    {
+        return [Message];
+    }
+
     public override HttpStatusCode GetStatusCode()
     {
         return HttpStatusCode.NotFound;
